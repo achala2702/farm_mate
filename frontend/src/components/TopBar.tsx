acef604311205d774246074c0ba60cc5f89e1ce6
achal2702 <achal2702@gmail.com>
2025-05-13 12:51:05 +0530

    add manual trigger for dark and light theme

--- frontend/src/components/TopBar.tsx
+++ frontend/src/components/TopBar.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import React from "react";
 import SearchInput from "./SearchInput";
 import { Icon } from "@iconify/react";
 import useScreenWidth from "@/hooks/useScreenWidth";
+import { useTheme } from "next-themes";
 
 type TopBarProps = {
   navbarOpen: boolean;
@@ -10,6 +13,8 @@ type TopBarProps = {
 
 export default function TopBar({ navbarOpen, setNavbarOpen }: TopBarProps) {
   const screenWidth = useScreenWidth();
+  const { theme, setTheme } = useTheme();
+
   return (
     <div className="flex px-14 items-center w-full justify-between py-2 border-b-1 border-gray-400 bg-background z-10 fixed">
       <div
@@ -29,9 +34,23 @@ export default function TopBar({ navbarOpen, setNavbarOpen }: TopBarProps) {
       <h1 className="text-primaryGreen text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold">
         FarmMate
       </h1>
-      <SearchInput width="w-64" className={`bg-custom-sidebar-hover ${screenWidth< 640? "hidden": ""}`} />
+      <SearchInput
+        width="w-64"
+        className={`bg-custom-sidebar-hover ${
+          screenWidth < 640 ? "hidden" : ""
+        }`}
+      />
       <div className="flex gap-4">
-        <p>d</p>
+        <button
+          className="cursor-pointer"
+          onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+        >
+          <Icon
+            icon={theme === "light" ? "solar:moon-linear" : "hugeicons:sun-03"}
+            width="24"
+            height="24"
+          />
+        </button>
         <p>d</p>
         <p>d</p>
       </div>
